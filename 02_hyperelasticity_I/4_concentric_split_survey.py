51e986783949029ded98325451c0471b6d68d894
Fabian Roth <fabian.roth.24@stud.tu-darmstadt.de>
2022-12-19 21:45:31 +0100

    Ready for Laptop :)

--- 02_hyperelasticity_I/4_concentric_split_survey.py
+++ 02_hyperelasticity_I/4_concentric_split_survey.py
@@ -32,12 +32,12 @@ def get_info_string(loss_weights):
 
 # %% Train multiple Models to average the results
 ICNN_params = {'loss_weights':        [1, 0],
-               'epochs':              10,
+               'epochs':              300,
                'learning_rate':       0.002,
                'weighted_load_cases': True,
                'model_size':          [8, 8]}
 
-FFNN_params = {'epochs':              10,
+FFNN_params = {'epochs':              6000,
                'learning_rate':       0.002,
                'weighted_load_cases': True,
                'model_size':          [32,32,16]}
@@ -146,6 +146,16 @@ for n, split in enumerate(train_test_split): # for every split calculate mean te
     losses['FFNN_test_avgs'].append(np.mean(test_loss_aggregate))
 
 plt.figure(1, dpi=600, figsize=(6,4))
+plt.scatter(losses['splits'], losses['ICNN_train_avgs'], color=dh.colors['o3'], label = 'ICNN training loss')
+plt.scatter(losses['splits'], losses['ICNN_test_avgs'], color=dh.colors['o5'], label = 'ICNN test loss')
+plt.scatter(losses['splits'], losses['FFNN_train_avgs'], color=dh.colors['b2'], label = 'FFNN training loss')
+plt.scatter(losses['splits'], losses['FFNN_test_avgs'], color=dh.colors['b4'], label = 'FFNN test loss')
+plt.xticks(losses['splits'])
+plt.xlabel('Training split')
+plt.ylabel('log$_{10}$ MSE')
+plt.title('Losses for different splits')
+plt.grid(which='both')
+plt.show()
 
 # %% Plot trainig losses
 for n, split in enumerate(train_test_split):
@@ -163,91 +173,92 @@ for n, split in enumerate(train_test_split):
     plt.ylabel('log$_{10}$ MSE')
     plt.title(f'Losses for split: {split}')
     plt.legend(['ICNN Training loss', 'ICNN Test loss', 'FFNN Training loss', 'FFNN Test loss'])
+    plt.show()
 
-# %% Compare Training and test losses
-# This is one of the ugliest code a human has written, sorry
-train_loss_aggregate = []
-test_loss_aggregate = []
-for r in ICNN_results:
-    train_loss_aggregate.append(r['train_loss'][0])
-    test_loss_aggregate.append(r['test_loss'][0])
-ICNN_train_loss_avg = np.mean(train_loss_aggregate)
-ICNN_train_loss_std = np.std(train_loss_aggregate)
-ICNN_test_loss_avg = np.mean(test_loss_aggregate)
-ICNN_test_loss_std = np.std(test_loss_aggregate)
+# # %% Compare Training and test losses
+# # This is one of the ugliest code a human has written, sorry
+# train_loss_aggregate = []
+# test_loss_aggregate = []
+# for r in ICNN_results:
+#     train_loss_aggregate.append(r['train_loss'][0])
+#     test_loss_aggregate.append(r['test_loss'][0])
+# ICNN_train_loss_avg = np.mean(train_loss_aggregate)
+# ICNN_train_loss_std = np.std(train_loss_aggregate)
+# ICNN_test_loss_avg = np.mean(test_loss_aggregate)
+# ICNN_test_loss_std = np.std(test_loss_aggregate)
 
-ICNN_loss_avg = [ICNN_train_loss_avg, ICNN_test_loss_avg]
-ICNN_loss_std = [ICNN_train_loss_std, ICNN_test_loss_std]
+# ICNN_loss_avg = [ICNN_train_loss_avg, ICNN_test_loss_avg]
+# ICNN_loss_std = [ICNN_train_loss_std, ICNN_test_loss_std]
 
-train_loss_aggregate = []
-test_loss_aggregate = []
-for r in FFNN_results:
-    train_loss_aggregate.append(r['train_loss'])
-    test_loss_aggregate.append(r['test_loss'])
-FFNN_train_loss_avg = np.mean(train_loss_aggregate)
-FFNN_train_loss_std = np.std(train_loss_aggregate)
-FFNN_test_loss_avg = np.mean(test_loss_aggregate)
-FFNN_test_loss_std = np.std(test_loss_aggregate)
+# train_loss_aggregate = []
+# test_loss_aggregate = []
+# for r in FFNN_results:
+#     train_loss_aggregate.append(r['train_loss'])
+#     test_loss_aggregate.append(r['test_loss'])
+# FFNN_train_loss_avg = np.mean(train_loss_aggregate)
+# FFNN_train_loss_std = np.std(train_loss_aggregate)
+# FFNN_test_loss_avg = np.mean(test_loss_aggregate)
+# FFNN_test_loss_std = np.std(test_loss_aggregate)
     
-FFNN_loss_avg = [FFNN_train_loss_avg, FFNN_test_loss_avg]
-FFNN_loss_std = [FFNN_train_loss_std, FFNN_test_loss_std]
+# FFNN_loss_avg = [FFNN_train_loss_avg, FFNN_test_loss_avg]
+# FFNN_loss_std = [FFNN_train_loss_std, FFNN_test_loss_std]
 
 
 
-x = np.arange(2)
-fig, ax = plt.subplots(dpi=600, figsize=(3,4))
-b1 = ax.bar(x-0.2, ICNN_loss_avg, yerr=ICNN_loss_std, label='ICNN loss',
-       align='center', ecolor=dh.colors['o3'], capsize=6, width=0.4, color=dh.colors['o4'])
-b2 = ax.bar(x+0.2, FFNN_loss_avg, yerr=FFNN_loss_std, label='FFNN loss',
-       align='center', ecolor=dh.colors['b3'], capsize=6, width=0.4, color=dh.colors['b2'])
-ax.set_xticks(x, ['Training', 'Test'], zorder=3)
-ax.grid(zorder=0)
-ax.set_axisbelow(True)
-plt.xticks(rotation='horizontal')
-plt.yscale('log')
-plt.legend(loc='upper left')
+# x = np.arange(2)
+# fig, ax = plt.subplots(dpi=600, figsize=(3,4))
+# b1 = ax.bar(x-0.2, ICNN_loss_avg, yerr=ICNN_loss_std, label='ICNN loss',
+#        align='center', ecolor=dh.colors['o3'], capsize=6, width=0.4, color=dh.colors['o4'])
+# b2 = ax.bar(x+0.2, FFNN_loss_avg, yerr=FFNN_loss_std, label='FFNN loss',
+#        align='center', ecolor=dh.colors['b3'], capsize=6, width=0.4, color=dh.colors['b2'])
+# ax.set_xticks(x, ['Training', 'Test'], zorder=3)
+# ax.grid(zorder=0)
+# ax.set_axisbelow(True)
+# plt.xticks(rotation='horizontal')
+# plt.yscale('log')
+# plt.legend(loc='upper left')
 
-# b1[1].set_color(dh.colors['o4'])
-# b2[1].set_color(dh.colors['b3'])
+# # b1[1].set_color(dh.colors['o4'])
+# # b2[1].set_color(dh.colors['b3'])
 
-plt.show()
+# plt.show()
 
-# %% Plot an example loadcase
+# # %% Plot an example loadcase
 
-lc_test = random.choice(test_list)
+# lc_test = random.choice(test_list)
 
-lc_icnn_model = ICNN_results[0]['model']
-lc_ffnn_model = FFNN_results[0]['model']
+# lc_icnn_model = ICNN_results[0]['model']
+# lc_ffnn_model = FFNN_results[0]['model']
 
-dh.plot_data(lc_test)
+# dh.plot_data(lc_test)
 
-ICNN_pred = {'P': lc_test['P']}
-ICNN_pred['*P'], _ = lc_icnn_model(lc_test['F'])
-ICNN_pred['*P'] = np.array(ICNN_pred['*P'])
-dh.plot_data(ICNN_pred, title=f'ICNN prediction for test load case {lc_test["load_case"]}',
-             tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
+# ICNN_pred = {'P': lc_test['P']}
+# ICNN_pred['*P'], _ = lc_icnn_model(lc_test['F'])
+# ICNN_pred['*P'] = np.array(ICNN_pred['*P'])
+# dh.plot_data(ICNN_pred, title=f'ICNN prediction for test load case {lc_test["load_case"]}',
+#              tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
 
-FFNN_pred = {'P': lc_test['P']}
-FFNN_pred['*P'] = lc_ffnn_model(lc_test['F'])
-FFNN_pred['*P'] = np.array(FFNN_pred['*P'])
-dh.plot_data(FFNN_pred, title=f'FFNN prediction for test load case {lc_test["load_case"]}',
-             tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
+# FFNN_pred = {'P': lc_test['P']}
+# FFNN_pred['*P'] = lc_ffnn_model(lc_test['F'])
+# FFNN_pred['*P'] = np.array(FFNN_pred['*P'])
+# dh.plot_data(FFNN_pred, title=f'FFNN prediction for test load case {lc_test["load_case"]}',
+#              tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
 
 
-lc_train = random.choice(train_list)
+# lc_train = random.choice(train_list)
 
-dh.plot_data(lc_train)
+# dh.plot_data(lc_train)
 
-lc_model = ICNN_results[0]['model']
-ICNN_pred = {'P': lc_train['P']}
-ICNN_pred['*P'], _ = lc_icnn_model(lc_train['F'])
-ICNN_pred['*P'] = np.array(ICNN_pred['*P'])
-dh.plot_data(ICNN_pred, title=f'ICNN prediction for train load case {lc_train["load_case"]}',
-             tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
+# lc_model = ICNN_results[0]['model']
+# ICNN_pred = {'P': lc_train['P']}
+# ICNN_pred['*P'], _ = lc_icnn_model(lc_train['F'])
+# ICNN_pred['*P'] = np.array(ICNN_pred['*P'])
+# dh.plot_data(ICNN_pred, title=f'ICNN prediction for train load case {lc_train["load_case"]}',
+#              tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
 
-lc_model = FFNN_results[0]['model']
-FFNN_pred = {'P': lc_train['P']}
-FFNN_pred['*P'] = lc_ffnn_model(lc_train['F'])
-FFNN_pred['*P'] = np.array(FFNN_pred['*P'])
-dh.plot_data(FFNN_pred, title=f'FFNN prediction for train load case {lc_train["load_case"]}',
-             tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
+# lc_model = FFNN_results[0]['model']
+# FFNN_pred = {'P': lc_train['P']}
+# FFNN_pred['*P'] = lc_ffnn_model(lc_train['F'])
+# FFNN_pred['*P'] = np.array(FFNN_pred['*P'])
+# dh.plot_data(FFNN_pred, title=f'FFNN prediction for train load case {lc_train["load_case"]}',
+#              tensor_kw={'legend': True}, dpi=600, figsize=(6,5))
